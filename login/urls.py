588f4cf419b2711006b04541267752f19e0f38fc
Sebastián Felipe Pérez Leal <felipeperezle@gmail.com>
2023-04-06 11:06:52 -0500

    urls add

--- login/urls.py
+++ login/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from home.views import index
 
 urlpatterns = [
     path('login/', views.login, name="login"),
     path('register/', views.register, name="register"),
     path('register/complete/', views.complete_registration, name="complete_registration"),
+    path('', index, name="home"),
 ]
