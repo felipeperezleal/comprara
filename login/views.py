ea45c0e05ea36f9a1de14c1310fd548d97c366b0
JuanDiego98 <jlozanoc@unal.edu.co>
2023-04-06 19:42:30 -0500

    Funcionalidad confirmar contraseña para registro

--- login/views.py
+++ login/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Client
 
 # Create your views here.
@@ -9,6 +10,10 @@ def register(request):
     return render(request, 'register.html')
 
 def complete_registration(request):
-    user = Client(email=request.POST['email'], password=request.POST['password'])
-    user.save()
-    return redirect('../../login/')
+    if request.POST.get('password') == request.POST.get('passwordConfirm'):
+        user = Client(email=request.POST['email'], password=request.POST['password'])
+        user.save()
+        return redirect('../../login/')
+    else:
+        messages.error(request, 'Las contraseñas no coinciden')
+        return render(request, 'register.html')
