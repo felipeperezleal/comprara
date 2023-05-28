b1229fc168aba992074cd0996b431d32019d641b
Felipe Pérez Leal <felipeperezle@gmail.com>
2023-05-27 21:34:24 -0500

    scraping updates

--- login/views.py
+++ login/views.py
@@ -33,12 +33,6 @@ def search(request):
 
 
 def scraping(store, url):
-    user_agent_list = [ 
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36', 
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15', 
-    ]
-    user_agent = random.choice(user_agent_list)
     r = requests.Session()
     soup = bs(r.get(url).content, "lxml")
     script = soup.find_all("script", {"type": "application/ld+json"})[2]
